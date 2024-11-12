b34d10650953368584b04160d262813c7b0d5db1
Panchenko Kateryna <buddhagolden@gmail.com>
2024-11-12 10:52:58 +0100

    header background position fixed

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,16 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const menuOpen = document.querySelector(".menu-open");
 const menuClose = document.querySelector(".menu-close");
 
-const toogleMenu = () => mobileMenu.classList.toggle("is-open");
+const toogleMenu = () => {
+  const isOpen = mobileMenu.classList.toggle("is-open");
+
+  // If the modal is now visible (isHidden is false), add no-scroll; otherwise, remove it
+  if (isOpen) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
+  }
+};
 
 menuOpen.addEventListener("click", toogleMenu);
 menuClose.addEventListener("click", toogleMenu);
--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,16 @@ const modalWindow = document.querySelector(".backdrop");
 const modalClose = document.querySelector(".modal-close");
 const modalOpen = document.querySelector(".modal-open");
 
-const toogleModal = () => modalWindow.classList.toggle("is-hidden");
+const toggleModal = () => {
+  const isHidden = modalWindow.classList.toggle("is-hidden");
 
-modalOpen.addEventListener("click", toogleModal);
-modalClose.addEventListener("click", toogleModal);
+  // If the modal is now visible (isHidden is false), add no-scroll; otherwise, remove it
+  if (!isHidden) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
+  }
+};
+
+modalOpen.addEventListener("click", toggleModal);
+modalClose.addEventListener("click", toggleModal);
