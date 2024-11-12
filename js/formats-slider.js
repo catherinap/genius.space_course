42e0c56cd17f3194ea2e48874b169037896755ba
Panchenko Kateryna <buddhagolden@gmail.com>
2024-11-12 01:55:27 +0100

    Cards Slider done using Swiper.js + continuation of adaptive layout

---
+++ js/formats-slider.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  breakpoints: {
+   320: {
+      slidesPerView: 1,
+    },
+    735: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+});
